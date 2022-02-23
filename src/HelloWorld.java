3e4fbf7a170e5f9c0db817b5db4b8cca6b26cf0a
BYARDHK21 <ByardHK21@gcc.edu>
2022-02-23 14:43:56 -0500

    Committed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Name please: ");
+        Scanner scnr = new Scanner(System.in);
+        String name = scnr.next();
+        System.out.println("hello " + name);
     }
+
 }
